6c85766134c2530df28baaead7836cfe24be0e84
시영 <bartimore00@naver.com>
2025-12-08 10:59:54 +0900

    Feat : styled 문법 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+// import './App.css'
+import styled from '@emotion/styled'
+import { StateComponent1 } from './exam/StateComponent1';
+
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Applogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+`;
+
+// interface PropsExample {
+//   name: String,
+//   color?: String
+// }
+
+// export const PropsExampleimpl = ({ name, color = "Blue" }: PropsExample) => {
+//   return PropsExampleimpl
+// }
 
 function App() {
+  const message = 'HELLO' // 더블쿼트 + 세미콜론 제거
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container>
+      <header className='App-header'>
+        <StateComponent1></StateComponent1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
-    </div>
-  );
+
+    </Container>
+  )
+
 }
 
-export default App;
+export default App
